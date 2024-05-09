a8be3d40678ddb7d16af54b6fd0f9eaa1830ecb5
Evelin Amorim <evelin.f.amorim@inescted.pt>
2024-05-09 10:18:05 +0100

    add distributed_state option to the training

--- arguments.py
+++ arguments.py
@@ -4,6 +4,7 @@ from typing import Optional, List, Union
 import torch
 from transformers import Seq2SeqTrainingArguments
 from trainer import OptimizerNames
+from accelerate import PartialState
 
 
 @dataclass
@@ -110,6 +111,8 @@ class CorefTrainingArguments(Seq2SeqTrainingArguments):
                            metadata={"help": "Whether to run training."})
     device: Optional[torch.device] = field(default=torch.device("cpu"),
                            metadata={"help": "Which device to use"})
+    distributed_state: Optional[PartialState] = field(default=PartialState(),
+                                                      metadata={"help":"store information about certain configurations or states"})
     save_dir: Optional[str] = field(
         default=None, metadata={"help": "Path to save predicts directory"}
     )
