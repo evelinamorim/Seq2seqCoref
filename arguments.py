8d4cc5ac7936217f194216ca10085f586e1f0ee7
Evelin Amorim <evelin.f.amorim@inescted.pt>
2024-05-10 15:18:19 +0100

    test the removal of deep_state of the training arguments

--- arguments.py
+++ arguments.py
@@ -5,7 +5,6 @@ import torch
 from transformers import Seq2SeqTrainingArguments
 from trainer import OptimizerNames
 
-from accelerate import PartialState
 
 @dataclass
 class ModelArguments:
@@ -111,8 +110,8 @@ class CorefTrainingArguments(Seq2SeqTrainingArguments):
                            metadata={"help": "Whether to run training."})
     device: Optional[torch.device] = field(default=torch.device("cpu"),
                            metadata={"help": "Which device to use"})
-    distributed_state: Optional[PartialState] = field(default=None,
-                                                      metadata={"help":"store information about certain configurations or states"})
+    #distributed_state: Optional[PartialState] = field(default=None,
+    #                                                  metadata={"help":"store information about certain configurations or states"})
     save_dir: Optional[str] = field(
         default=None, metadata={"help": "Path to save predicts directory"}
     )
